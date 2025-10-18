346065274927e1f2e8dfd0cf7d045e835659b6e4
New9c <aimccccccccc@gmail.com>
2025-10-18 19:51:28 +0800

    control twitch msg print

--- twitch.py
+++ twitch.py
@@ -9,7 +9,8 @@ def show_twitch_msg(message):
     if message["display-name"] not in SETTINGS["twitch_ignore_names"]:
         message_frames = 90
         twitch_msg = f"{message["display-name"]}: {message["message"]}"
-        print(twitch_msg)
+        if SETTINGS['print_twitch_msg']:
+            print(twitch_msg)
         if len(twitch_msg)>SETTINGS["text_len"]:
             too_big_string = f"-[>{SETTINGS["text_len"]}]"
             twitch_msg = f"{twitch_msg[:SETTINGS["text_len"]-len(too_big_string)]}{too_big_string}"
